e9071e788dbf87c12ab933a2d005cd51dce18f1e
Olivia Stewart <liv.stewart2004@gmail.com>
2024-07-04 18:58:22 +1000

    update db

---
+++ HMS.Data/HMSDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMS.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HMS.Data
+{
+    internal class HMSDbContext : DbContext 
+    {
+        public string DbPath { get; }
+        public DbSet<RoleModel> Roles { get; set; }
+
+
+        public HMSDbContext()
+        {
+	        var folder = Environment.SpecialFolder.LocalApplicationData;
+	        var path = Environment.GetFolderPath(folder);
+	        DbPath = System.IO.Path.Join(path, "HMS.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+	        => options.UseSqlite($"Data Source={DbPath};");
+    }
+}
---
+++ HMS.Data/Migrations/20240704085807_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RM_Id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RM_Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
